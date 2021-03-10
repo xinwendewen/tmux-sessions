73070578cb60e18e07d6db78250a0883b41f986f
xinwendewen <zwt0909@outlook.com>
2021-03-11 00:59:30 +0800

    merge adb and sys metrics board

--- metrics-board.sh
+++ metrics-board.sh
@@ -8,12 +8,11 @@ CURRENT_DIR="$(cd "$(dirname $BASH_SOURCE[0])" && pwd)"
 source ${CURRENT_DIR}/tmux-functions.sh
 source ${CURRENT_DIR}/adb-metrics.conf
 
-echo "setting up adb metrics board tmux session"
+echo "setting up metrics board tmux session"
 
-readonly SESSION="adb-metrics-board"
+readonly SESSION="metrics-board"
 
-main() {
-    create_session "${SESSION}"
+setup_adb_metrics_window() {
     kill_window "=${SESSION}:3"
     create_window "=${SESSION}:3" "adb"
     if [[ -n ${HOST} && -n ${PORT} ]]; then
@@ -21,6 +20,20 @@ main() {
     else
         send_command "=${SESSION}:3.1" "watch adb devices" C-m
     fi
+}
+
+setup_sys_metrics_window() {
+    kill_window "=${SESSION}:2"
+    create_window "=${SESSION}:2" "sys"
+    send_command "=${SESSION}:2" "top"
+    split_vertical "=${SESSION}:2"
+    send_command "=${SESSION}:2.2" "watch -n 10 df -h" C-m
+}
+
+main() {
+    create_session "${SESSION}"
+    setup_sys_metrics_window
+    setup_adb_metrics_window
 }
 
 main
